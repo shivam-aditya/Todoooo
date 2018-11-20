b2cdecff63705b629de8f8736ee117583264a3af
Shivam Aditya <shivam.aditya@verse.in>
2018-11-20 12:05:58 +0530

    Basic todo app done

---
+++ Todoooo/Custom Cell/CustomListCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomMessageCell.swift
+//  Flash Chat
+//
+//  Created by Angela Yu on 30/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CustomListCell: UITableViewCell {
+
+    @IBOutlet weak var messageBackground: UIView!
+    @IBOutlet weak var messageBody: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code goes here
+        
+        
+        
+    }
+
+
+}
---
+++ Todoooo/TodoListViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Todoooo
+//
+//  Created by Shivam Aditya on 19/11/18.
+//  Copyright © 2018 Shivam Aditya. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find chotu", "Kill Boss", "Buy butcher knife for killing boss", "Torture him"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //tableView.register(UINib(nibName: "ToDoItemCell", bundle: nil), forCellReuseIdentifier: "toDoItemCell")
+    }
+    
+    //MARK - Tableview Datasource Methods
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath) //as! CustomListCell
+        
+        //cell.messageBody.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Tapped item is \(itemArray[indexPath.row])")
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        if let accessoryType = cell?.accessoryType {
+            if(accessoryType != .none){
+                cell?.accessoryType = .none
+            }
+            else{
+                cell?.accessoryType = .checkmark
+            }
+        }
+        else{
+            //not used
+            cell?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
